626a252d9e773bd5fcd46f5fd0ca0757b9b17397
Alexey Romanov <alexey.acid@gmail.com>
2011-05-07 14:02:25 +0400

    update demo README and osx-open-all-*.sh for EZPlatformer demo

--- etc/osx-open-all-debug.sh
+++ etc/osx-open-all-debug.sh
@@ -4,3 +4,4 @@ open test/FluintLuaAlchemyTests/build/debug/FluintLuaAlchemyTests.swf
 open demo/LuaAlchemyDemo/build/debug/LuaAlchemyDemo.swf
 open demo/MiniDemo/build/debug/MiniDemo.swf
 open demo/Rapid/build/debug/Rapid.swf
+open demo/EZPlatformer/build/debug/EZPlatformerLuaAlchemy.swf
--- etc/osx-open-all-release.sh
+++ etc/osx-open-all-release.sh
@@ -4,3 +4,4 @@ open test/FluintLuaAlchemyTests/build/release/FluintLuaAlchemyTests.swf
 open demo/LuaAlchemyDemo/build/release/LuaAlchemyDemo.swf
 open demo/MiniDemo/build/release/MiniDemo.swf
 open demo/Rapid/build/release/Rapid.swf
+open demo/EZPlatformer/build/release/EZPlatformerLuaAlchemy.swf
